7cee85328a34a27984e766935036f2a1743aaa32
rordev456 <devidev45@proton.me>
2018-12-07 11:24:15 -0500

    Created reviews model and forced referential integrity between reviews products and users

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ActiveRecord::Base
+  belongs_to :product, class_name: "Product", foreign_key: 'product_id'
+  belongs_to :user, class_name: "User", foreign_key: 'user_id'
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :reviews, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :first_name , presence: true
   validates :last_name , presence: true
